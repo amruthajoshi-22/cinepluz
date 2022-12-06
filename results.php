3d870e10bae799a54efda4e46d073ffaaafb8ee7
amrutha joshi <amruthajoshi2002@gmail.com>
2022-12-06 17:10:36 +0530

    sixth Commit

--- results.php
+++ results.php
@@ -14,9 +14,31 @@ if(isset($_POST['quizsubmit'])){
     if(!empty($_POST['quizcheck'])){
         $count=count($_POST['quizcheck']);
 
-        echo "Out of 10, you have selected  ".$count."  options";
-        $selected =$_POST['quizcheck'];
+        echo "Out of 10, you have selected  ".$count."options";
+        $result=0;
+        $i=0;
+        $k=1;
+        $selected[$i++] =$_POST['quizcheck'];
         print_r($selected);
+
+        $q="select * from strangerthings_qns";
+        $query=mysqli_query($con,$q);
+
+        while( $rows=mysqli_fetch_array($query)){
+            print_r($rows['ans_id']);
+            echo " ";
+
+            $checked= $rows['ans_id']== $selected[$k];
+            if($checked){
+                $result++;
+            }
+            $k++;
+            
+        }
+        ?>
+          <br>
+        <?php
+        echo "$result";
     }
 }
 else{
